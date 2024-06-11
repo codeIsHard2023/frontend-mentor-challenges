e54b40348aae0b7e6c0fa47798a87aa3926e2ac6
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2024-06-11 16:06:48 +0200

    main branch clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import ImageQrCode from "./assets/image-qr-code.png";
 import "./App.css";
 
 function App() {
   return (
-    <section id="qrSection">
-      <div className="card">
-        <div className="qrCode">
-          <img className="imageQrCode" src={ImageQrCode} alt="QR code" />
-        </div>
-        <div className="textContainer">
-          <p>Improve your front-end skills by building projects</p>
-          <p>
-            Scan the QR code to visit Frontend Mentor and take your coding
-            skills to the next level
-          </p>
-        </div>
-      </div>
-    </section>
+
   );
 }
 
