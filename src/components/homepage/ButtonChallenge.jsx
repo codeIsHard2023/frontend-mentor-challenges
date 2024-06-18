5843e0d5768ca0a9b8d7f53995d41163c9060fe5
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2024-06-18 18:41:50 +0200

    new: homepage with bootstrap header and buttons for each challenge

---
+++ src/components/homepage/ButtonChallenge.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../styles/homepage/Button.css";
+import Button from "react-bootstrap/Button";
+
+// eslint-disable-next-line react/prop-types
+function ButtonChallenge({ challenge }) {
+  const [challengeChoice, setChallengeChoice] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChallengeSelection = () => {
+    setChallengeChoice(challenge);
+  };
+
+  const changeFormatChallengeChoice = (challengeStringtoFormat) => {
+    return challengeStringtoFormat.toLowerCase().split(" ").join("-");
+  };
+
+  useEffect(() => {
+    if (challengeChoice) {
+      navigate(`/challenge-${changeFormatChallengeChoice(challengeChoice)}`);
+    }
+  }, [challengeChoice, navigate]);
+
+  const handleClick = () => {
+    handleChallengeSelection();
+  };
+
+  return (
+    <Button type="button" onClick={handleClick}>
+      {challenge}{" "}
+    </Button>
+  );
+}
+
+export default ButtonChallenge;
